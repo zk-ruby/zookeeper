d42e1a7f9120386cac1ed67a8a78c24f89a671d3
slyphon <simms@hp.com>
2011-02-08 20:00:13 +0000

    added get_children specs

--- spec/zookeeper_spec.rb
+++ spec/zookeeper_spec.rb
@@ -11,20 +11,20 @@ shared_examples_for "all success return values" do
 end
 
 describe Zookeeper do
+  before do
+    @path = "/_zktest_"
+    @data = "underpants"
+
+    @zk.create(:path => @path, :data => @data)
+  end
+
+  after do
+    @zk.delete(:path => @path)
+  end
+
   # unfortunately, we can't test w/o exercising other parts of the driver, so
   # if "set" is broken, this test will fail as well (but whaddyagonnado?)
   describe :get do
-    before do
-      @path = "/_zktest_"
-      @data = "underpants"
-
-      @zk.create(:path => @path, :data => @data)
-    end
-
-    after do
-      @zk.delete(:path => @path)
-    end
-
     describe :sync do
       it_should_behave_like "all success return values"
 
@@ -65,6 +65,7 @@ describe Zookeeper do
         @watcher.path.should == @path
         @watcher.context.should == @path
         @watcher.should be_completed
+        @watcher.type.should == Zookeeper::ZOO_CHANGED_EVENT
       end
     end
 
@@ -133,16 +134,8 @@ describe Zookeeper do
 
   describe :set do
     before do
-      @path = "/_zktest_"
-      @orig_data = "underpants"
       @new_data = "Four score and \007 years ago"
-
-      @zk.create(:path => @path, :data => @orig_data)
       @stat = @zk.stat(:path => @path)[:stat]
-    end
-
-    after do
-      @zk.delete(:path => @path)
     end
 
     describe :sync do
@@ -256,6 +249,162 @@ describe Zookeeper do
           @cb.stat.exists.should be_false
         end
       end
+    end
+  end
+
+  describe :get_children do
+    before do
+      @children = %w[child0 child1 child2]
+
+      @children.each do |name|
+        @zk.create(:path => "#{@path}/#{name}", :data => name)
+      end
+    end
+
+    after do
+      @children.each do |name|
+        @zk.delete(:path => "#{@path}/#{name}")
+      end
+    end
+
+    describe :sync do
+      it_should_behave_like "all success return values"
+
+      before do
+        @rv = @zk.get_children(:path => @path)
+      end
+
+      it %[should have an array of names of the children] do
+        @rv[:children].should be_kind_of(Array)
+        @rv[:children].length.should == 3
+        @rv[:children].sort.should == @children.sort 
+      end
+
+      # "Three shall be the number of the counting, and the number of the counting shall be 3"
+
+      it %[should have a stat object whose num_children is 3] do
+        @rv[:stat].should_not be_nil
+        @rv[:stat].should be_kind_of(ZookeeperStat::Stat)
+        @rv[:stat].num_children.should == 3
+      end
+    end
+
+    describe :sync_watch do
+      it_should_behave_like "all success return values"
+
+      before do
+        @addtl_child = 'child3'
+
+        @watcher = Zookeeper::WatcherCallback.new
+
+        @rv = @zk.get_children(:path => @path, :watcher => @watcher, :watcher_context => @path)
+      end
+
+      after do
+        @zk.delete(:path => "#{@path}/#{@addtl_child}")
+      end
+
+      it %[should have an array of names of the children] do
+        @rv[:children].should be_kind_of(Array)
+        @rv[:children].length.should == 3
+        @rv[:children].sort.should == @children.sort 
+      end
+
+      it %[should have a stat object whose num_children is 3] do
+        @rv[:stat].should_not be_nil
+        @rv[:stat].should be_kind_of(ZookeeperStat::Stat)
+        @rv[:stat].num_children.should == 3
+      end
+
+      it %[should set a watcher for children on the node] do
+        @watcher.should_not be_completed
+
+        @zk.create(:path => "#{@path}/#{@addtl_child}", :data => '')[:rc].should == Zookeeper::ZOK
+
+        wait_until { @watcher.completed? }
+        @watcher.should be_completed
+
+        @watcher.path.should == @path
+        @watcher.context.should == @path
+        @watcher.type.should == Zookeeper::ZOO_CHILD_EVENT
+      end
+    end
+
+    describe :async do
+      it_should_behave_like "all success return values"
+
+      before do
+        @cb = ZookeeperCallbacks::StringsCallback.new
+        @rv = @zk.get_children(:path => @path, :callback => @cb, :callback_context => @path)
+
+        wait_until { @cb.completed? }
+        @cb.should be_completed
+      end
+
+      it %[should succeed] do
+        @cb.return_code.should == Zookeeper::ZOK
+      end
+
+      it %[should return an array of children] do
+        @cb.children.should be_kind_of(Array)
+        @cb.children.length.should == 3
+        @cb.children.sort.should == @children.sort 
+      end
+
+      it %[should have a stat object whose num_children is 3] do
+        @cb.stat.should_not be_nil
+        @cb.stat.should be_kind_of(ZookeeperStat::Stat)
+        @cb.stat.num_children.should == 3
+      end
+    end
+
+    describe :async_watch do
+      it_should_behave_like "all success return values"
+
+      before do
+        @addtl_child = 'child3'
+
+        @watcher = Zookeeper::WatcherCallback.new
+        @cb = ZookeeperCallbacks::StringsCallback.new
+
+        @rv = @zk.get_children(:path => @path, :watcher => @watcher, :watcher_context => @path, :callback => @cb, :callback_context => @path)
+        wait_until { @cb.completed? }
+        @cb.should be_completed
+      end
+
+      after do
+        @zk.delete(:path => "#{@path}/#{@addtl_child}")
+      end
+
+      it %[should succeed] do
+        @cb.return_code.should == Zookeeper::ZOK
+      end
+
+      it %[should return an array of children] do
+        @cb.children.should be_kind_of(Array)
+        @cb.children.length.should == 3
+        @cb.children.sort.should == @children.sort 
+      end
+
+      it %[should have a stat object whose num_children is 3] do
+        @cb.stat.should_not be_nil
+        @cb.stat.should be_kind_of(ZookeeperStat::Stat)
+        @cb.stat.num_children.should == 3
+      end
+
+      it %[should set a watcher for children on the node] do
+        @watcher.should_not be_completed
+
+        @zk.create(:path => "#{@path}/#{@addtl_child}", :data => '')[:rc].should == Zookeeper::ZOK
+
+        wait_until { @watcher.completed? }
+        @watcher.should be_completed
+
+        @watcher.path.should == @path
+        @watcher.context.should == @path
+        @watcher.type.should == Zookeeper::ZOO_CHILD_EVENT
+      end
+
     end
   end
 end
