c83d794afa6c97fa11540590efcfbc8ed7c21b07
Jonathan D. Simms <slyphon@gmail.com>
2012-04-19 21:14:56 +0000

    get rid of em_spec's selectable_io calls, no need

--- spec/em_spec.rb
+++ spec/em_spec.rb
@@ -28,40 +28,6 @@ describe 'ZookeeperEM' do
       end
     end
 
-    describe 'selectable_io' do
-      it %[should return an IO object] do
-        setup_zk do
-          @zk.selectable_io.should be_instance_of(IO)
-          teardown_and_done
-        end
-      end
-
-      it %[should not be closed] do
-        setup_zk do
-          @zk.selectable_io.should_not be_closed
-          teardown_and_done
-        end
-      end
-
-      before do
-        @data_cb = ZookeeperCallbacks::DataCallback.new do
-          logger.debug { "cb called: #{@data_cb.inspect}" }
-        end
-      end
-
-      it %[should be read-ready if there's an event waiting] do
-        setup_zk do
-          @zk.get(:path => "/", :callback => @data_cb)
-
-          r, *_ = IO.select([@zk.selectable_io], [], [], 2)
-
-          r.should be_kind_of(Array)
-
-          teardown_and_done
-        end
-      end
-    end
-
     describe 'callbacks' do
       it %[should be called on the reactor thread] do
         cb = lambda do |h|
