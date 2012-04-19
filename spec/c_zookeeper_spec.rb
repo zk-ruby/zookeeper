f8e65510e09ace2a74bc07253df52a7a41f18aa0
Jonathan D. Simms <slyphon@gmail.com>
2012-04-19 03:50:06 +0000

    check for our connection event after zkc_state says we're connected

--- spec/c_zookeeper_spec.rb
+++ spec/c_zookeeper_spec.rb
@@ -16,27 +16,33 @@ if Module.const_defined?(:CZookeeper)
       wait_until(timeout) { @czk.state == ZookeeperConstants::ZOO_CONNECTED_STATE }
     end
 
-    before do
-      @event_queue = ZookeeperCommon::QueueWithPipe.new
-      @czk = CZookeeper.new('localhost:2181', @event_queue)
-    end
-
-    after do
-      @czk.close rescue Exception
-      @event_queue.close rescue Exception
-    end
-
-    it %[should be in connected state within a reasonable amount of time] do
-      wait_until_connected.should be_true
-    end
-
-    describe :after_connected do
+    describe do
       before do
+        @event_queue = ZookeeperCommon::QueueWithPipe.new
+        @czk = CZookeeper.new('localhost:2181', @event_queue)
+      end
+
+      after do
+        @czk.close rescue Exception
+        @event_queue.close rescue Exception
+      end
+
+      it %[should be in connected state within a reasonable amount of time] do
         wait_until_connected.should be_true
       end
 
-      it %[should have a connection event after being connected] do
+      describe :after_connected do
+        before do
+          wait_until_connected.should be_true
+        end
 
+        it %[should have a connection event after being connected] do
+          event = wait_until(2) { @event_queue.pop }
+          event.should be
+          event[:req_id].should == ZookeeperCommon::ZKRB_GLOBAL_CB_REQ
+          event[:type].should   == ZookeeperConstants::ZOO_SESSION_EVENT
+          event[:state].should  == ZookeeperConstants::ZOO_CONNECTED_STATE
+        end
       end
     end
   end
