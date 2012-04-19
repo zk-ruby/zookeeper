130cfb296f7828c276ce5cc98db40668a13403df
Jonathan D. Simms <slyphon@gmail.com>
2012-04-19 03:28:05 +0000

    change check_debug to log through zkrb_debug

--- ext/dbg.h
+++ ext/dbg.h
@@ -28,7 +28,7 @@
 
 #define check_mem(A) check((A), "Out of memory.")
 
-#define check_debug(A, M, ...) if(!(A)) { debug(M, ##__VA_ARGS__); errno=0; goto error; } 
+#define check_debug(A, M, ...) if(!(A)) { zkrb_debug(M, ##__VA_ARGS__); errno=0; goto error; } 
 
 #define zkrb_debug(M, ...) if (ZKRBDebugging) fprintf(stderr, "DEBUG %s:%d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 #define zkrb_debug_inst(O, M, ...) zkrb_debug("obj_id: %lx, " M, FIX2LONG(rb_obj_id(O)), ##__VA_ARGS__)
