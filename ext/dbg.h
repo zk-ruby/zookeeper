fd290aad618587f7218c803cfa3122d769f11330
Jonathan D. Simms <slyphon@gmail.com>
2012-04-20 13:35:39 +0000

    some useful additions to dbg.h

--- ext/dbg.h
+++ ext/dbg.h
@@ -22,15 +22,28 @@
 
 #define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d) " M "\n", __FILE__, __LINE__, ##__VA_ARGS__) 
 
+// acts to assert that A is true
 #define check(A, M, ...) if(!(A)) { log_err(M, ##__VA_ARGS__); errno=0; goto error; } 
+
+// like check, but provide an explicit goto label name
+#define check_goto(A, L, M, ...) if(!(A)) { log_err(M, ##__VA_ARGS__); errno=0; goto L; } 
 
 #define sentinel(M, ...)  { log_err(M, ##__VA_ARGS__); errno=0; goto error; } 
 
 #define check_mem(A) check((A), "Out of memory.")
 
-#define check_debug(A, M, ...) if(!(A)) { zkrb_debug(M, ##__VA_ARGS__); errno=0; goto error; } 
+// checks the condition A, if not true, logs the message M given using zkrb_debug
+// then does a goto to the label L 
+#define check_debug_goto(A, L, M, ...) if(!(A)) { zkrb_debug(M, ##__VA_ARGS__); errno=0; goto L; } 
 
-#define zkrb_debug(M, ...) if (ZKRBDebugging) fprintf(stderr, "DEBUG %s:%d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+// check_debug_goto with implicit 'unlock' label
+#define check_debug_unlock(A, M, ...) check_debug_goto(A, unlock, M, ##__VA_ARGS__) 
+
+// like check_debug_goto, but the label is implicitly 'error'
+#define check_debug(A, M, ...) check_debug_goto(A, error, M, ##__VA_ARGS__) 
+
+
+#define zkrb_debug(M, ...) if (ZKRBDebugging) fprintf(stderr, "DEBUG %p:%s:%d: " M "\n", pthread_self(), __FILE__, __LINE__, ##__VA_ARGS__)
 #define zkrb_debug_inst(O, M, ...) zkrb_debug("obj_id: %lx, " M, FIX2LONG(rb_obj_id(O)), ##__VA_ARGS__)
 
 #endif
