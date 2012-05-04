3d1bc6339f43ae4085c983e4e91ee460f2094bce
slyphon <slyphon@gmail.com>
2012-05-04 02:34:21 +0000

    cruft removal
    these were original tests and examples that haven't been maintained or used in ages

--- test/test_esoteric.rb
+++
@@ -1,7 +0,0 @@
-require 'rubygems'
-require 'zookeeper'
-zk = Zookeeper.new('localhost:2181')
-
-puts "get acl #{zk.get_acl(:path => '/').inspect}"
-puts "zerror #{zk.zerror(Zookeeper::ZBADVERSION)}"
-
