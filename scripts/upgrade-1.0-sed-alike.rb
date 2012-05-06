f852523c05ca08e3056c3e2e1bc77cc50ba75f3f
slyphon <slyphon@gmail.com>
2012-05-06 02:34:58 +0000

    handle the relative reference to ZookeeperException

--- scripts/upgrade-1.0-sed-alike.rb
+++ scripts/upgrade-1.0-sed-alike.rb
@@ -31,7 +31,9 @@ ARGV.each do |path|
           gsub(/\bZookeeper::(\w+)Callback\b/, 'Zookeeper::Callbacks::\1Callback').
           gsub(/\bZookeeperACLs::(ZOO_\w+)\b/, 'Zookeeper::Constants::\1').
           gsub(/\bZookeeperExceptions::ZookeeperException::(\w+)\b/, 'Zookeeper::Exceptions::\1').
-          gsub(/\bZookeeper(Constants|Exceptions|Common|ACLs|Callbacks)\b/, 'Zookeeper::\1')
+          gsub(/\bZookeeper(Constants|Exceptions|Common|ACLs|Callbacks)\b/, 'Zookeeper::\1').
+          gsub(/\bZookeeperException::(\w+)\b/, 'Exceptions::\1')
+
       end
     end
 
