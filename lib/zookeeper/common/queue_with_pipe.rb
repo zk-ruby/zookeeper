ce6ad2d055ccc6c3c56d591747ee01a1a6eb350d
Jonathan D. Simms <slyphon@twitter.com>
2012-10-08 20:56:47 +0000

    fix queue_with_pipe to timeout waiting so we don't hang forever in psychotic conditions

--- lib/zookeeper/common/queue_with_pipe.rb
+++ lib/zookeeper/common/queue_with_pipe.rb
@@ -12,40 +12,35 @@ module Common
     # @private
     KILL_TOKEN = Object.new unless defined?(KILL_TOKEN)
 
+    # @private
+    COND_WAIT_TIMEOUT = 5
+
     def initialize
       @array = []
 
-      @mutex    = Mutex.new
-      @cond     = ConditionVariable.new
+      @mutex    = Monitor.new
+      @cond     = @mutex.new_cond
       @closed   = false
       @graceful = false
     end
 
     def clear
-      @mutex.lock
-      begin
+      @mutex.synchronize do
         @array.clear
-      ensure
-        @mutex.unlock rescue nil
       end
     end
 
     def push(obj)
-      @mutex.lock
-      begin
-#         raise ShutdownException if (@closed or @graceful)
+      @mutex.synchronize do
         @array << obj
         @cond.signal
-      ensure
-        @mutex.unlock rescue nil
       end
     end
 
     def pop(non_blocking=false)
       rval = nil
 
-      @mutex.lock
-      begin
+      @mutex.synchronize do
 
         begin
           raise ShutdownException if @closed  # this may get us in trouble
@@ -56,49 +51,38 @@ module Common
             raise ThreadError if non_blocking     # sigh, ruby's stupid behavior
             raise ShutdownException if @graceful  # we've processed all the remaining mesages
 
-            @cond.wait(@mutex) until (@closed or @graceful or (@array.length > 0))
+            @cond.wait(COND_WAIT_TIMEOUT) until (@closed or @graceful or (@array.length > 0))
           end
         end until rval
 
         return rval
 
-      ensure
-        @mutex.unlock rescue nil
       end
     end
 
     # close the queue and causes ShutdownException to be raised on waiting threads
     def graceful_close!
-      @mutex.lock
-      begin
+      @mutex.synchronize do
         return if @graceful or @closed
         logger.debug { "#{self.class}##{__method__} gracefully closing" }
         @graceful = true
         @cond.broadcast
-      ensure
-        @mutex.unlock rescue nil
       end
       nil
     end
 
     def open
-      @mutex.lock
-      begin
+      @mutex.synchronize do
         @closed = @graceful = false
         @cond.broadcast
-      ensure
-        @mutex.unlock rescue nil
       end
     end
 
     def close
-      @mutex.lock
-      begin
+      @mutex.synchronize do
         return if @closed
         @closed = true
         @cond.broadcast
-      ensure
-        @mutex.unlock rescue nil
       end
     end
 
