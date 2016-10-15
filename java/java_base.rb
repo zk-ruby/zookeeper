addd536ec440f7c51cd51153bf61fa80ece97978
Lex Spoon <lex@squareup.com>
2016-10-15 18:37:47 -0400

    Fix a typo on a has_key

--- java/java_base.rb
+++ java/java_base.rb
@@ -490,7 +490,7 @@ class JavaBase
 
     def replace_jzk!(opts = {})
       orig_jzk = @jzk
-      if opts.has_key?(:session_id) && opts.has_key(:session_passwd)
+      if opts.has_key?(:session_id) && opts.has_key?(:session_passwd)
         @jzk = JZK::ZooKeeper.new(@host, DEFAULT_SESSION_TIMEOUT, JavaCB::WatcherCallback.new(event_queue, :client => self), opts.fetch(:session_id), opts.fetch(:session_passwd).to_java_bytes)
       else
         @jzk = JZK::ZooKeeper.new(@host, DEFAULT_SESSION_TIMEOUT, JavaCB::WatcherCallback.new(event_queue, :client => self))
